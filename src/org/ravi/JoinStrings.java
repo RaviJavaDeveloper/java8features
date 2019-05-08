7c5756412910e8a8bfb5d00f9df2d47bc6acad3c
Ravi Kumar Sukola <ravikumar.sukola@cytrion.com>
2019-05-08 19:04:07 +0530

    Added optional class examples

---
+++ src/org/ravi/JoinStrings.java
@@ -0,0 +1,18 @@
+package org.ravi;
+
+import java.util.List;
+import java.util.StringJoiner;
+
+public class JoinStrings {
+
+    public static void main(String[] args) {
+        StringJoiner names = new StringJoiner(", ","[","]");
+        names.setEmptyValue("Currently no names available");
+        System.out.println(names);
+        System.out.println("Adding names now.");
+        List<User> users = UsersListFactory.getList();
+        users.stream().map(u->u.getName()).sorted().forEach(names::add);
+        System.out.println(names);
+        System.out.println("Length is "+names.length());
+    }
+}
